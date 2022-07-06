3b55dfd4295c16e6b31499438d805a83d4f67a42
Florent de Labarre <florent.mirieu@gmail.com>
2022-07-06 14:08:06 +0200

    [FIX] sale: prevent -0.0 total
    In some circonstance the amount can be -0.00, show 0.00 is more logical

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -47,9 +47,9 @@ class SaleOrder(models.Model):
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
             order.update({
-                'amount_untaxed': amount_untaxed,
-                'amount_tax': amount_tax,
-                'amount_total': amount_untaxed + amount_tax,
+                'amount_untaxed': amount_untaxed + 0.0,
+                'amount_tax': amount_tax + 0.0,
+                'amount_total': amount_untaxed + amount_tax + 0.0,
             })
 
     @api.depends('order_line.invoice_lines')
